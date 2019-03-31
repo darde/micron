c2d0dcf1caca38222f3e7819d8e8ab78239456ce
darde <pablodarde@gmail.com>
2019-03-30 21:10:53 -0300

    Adds search clean button

--- components/search/index.js
+++ components/search/index.js
@@ -9,7 +9,7 @@ var getMicronSearch = function() {
 
   var inputSearch = createElement({
     type: 'input',
-    inputType: 'search',
+    inputType: 'text',
     class: 'input-field',
     placeholder: 'Search',
   });
@@ -26,20 +26,40 @@ var getMicronSearch = function() {
     class: 'search-container-right',
   });
 
+  var searchClean = createElement({
+    class: 'search-clean',
+  });
+
+  var searchCleanButton = createElement({
+    type: 'button',
+    id: 'clean-search-btn',
+  });
+
   searchContainer.appendChild(glassHolder);
   searchContainer.appendChild(searchContainerLeft);
   searchContainer.appendChild(inputSearch);
+  searchClean.appendChild(searchCleanButton);
+  searchContainer.appendChild(searchClean);
   searchContainer.appendChild(searchContainerRight);
 
   function openSearch() {
     searchContainer.classList.add('open');
     inputSearch.focus();
     inputSearch.addEventListener('blur', closeSearch);
+    searchCleanButton.addEventListener('mousedown', cleanSearch);
   }
 
   function closeSearch(e) {
     searchContainer.classList.remove('open');
     e.target.removeEventListener('blur', closeSearch);
+    searchCleanButton.removeEventListener('click', cleanSearch);
+  }
+
+  function cleanSearch(e) {
+    console.log(e.target.id);
+    e.stopImmediatePropagation();
+    inputSearch.removeEventListener('blur', closeSearch);
+    inputSearch.value = '';
   }
 
   searchContainer.addEventListener('click', openSearch);
